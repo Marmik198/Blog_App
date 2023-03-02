ffa57bb4d1107ebff5fd187765f9f0d5cc19027c
Marmik198 <shahmarmik1@gmail.com>
2023-03-03 01:06:41 +0530

    Media Files

--- Blog_App/settings.py
+++ Blog_App/settings.py
@@ -136,6 +136,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
-# 
+# Login URLS
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = "login"
+
+# Media Files
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
