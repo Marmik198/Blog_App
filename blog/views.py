e3bf35c06e53cf2ac5592697975759320217bed5
Marmik198 <shahmarmik1@gmail.com>
2023-03-04 10:19:08 +0530

    Added ListView for Posts

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import (
+    ListView,
+    UpdateView,
+    CreateView,
+    DeleteView,
+    DetailView
+)
+from pprint import pprint
 from .models import Post
 
 # Home View
@@ -7,6 +15,12 @@ def home(request):
         'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
 
 # About View
 def about(request):
