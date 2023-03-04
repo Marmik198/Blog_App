887b8d5b54eba1f6cccc7e67f8f6bb1db605b868
Marmik198 <shahmarmik1@gmail.com>
2023-03-04 11:58:46 +0530

    Addded Delete and Update View

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.views.generic import (
     ListView,
     UpdateView,
@@ -31,10 +32,28 @@ class PostDetailView(DetailView):
     model = Post
 
 # Post Create View
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ["title", "content"]
 
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+# Post Update View
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ["title", "content"]
+    
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+# Post Delete View
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
